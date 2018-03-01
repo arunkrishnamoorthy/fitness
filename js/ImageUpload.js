6d6b0cfdf6f9494fa3a44ddef4c68f15792f6039
arunkrishnamoorthy <arunmba.rec@gmail.com>
2018-03-01 14:10:09 +0530

    Initial Commit - Fitness App

---
+++ js/ImageUpload.js
@@ -0,0 +1,22 @@
+(function ($) {
+    "use strict";
+
+    var $imageupload = $('.imageupload');
+    $imageupload.imageupload();
+
+    $('#imageupload-disable').on('click', function() {
+        $imageupload.imageupload('disable');
+        $(this).blur();
+    })
+
+    $('#imageupload-enable').on('click', function() {
+        $imageupload.imageupload('enable');
+        $(this).blur();
+    })
+
+    $('#imageupload-reset').on('click', function() {
+        $imageupload.imageupload('reset');
+        $(this).blur();
+    });
+
+})(jQuery);
---
+++ js/Registration.js
@@ -0,0 +1,32 @@
+/**
+ * Class Constructor
+ */
+function RegisterUser(){
+    return this;
+}
+
+RegisterUser.prototype.AddUser = function(oData){
+    debugger;
+    window.EasyLoading.show({
+        type: EasyLoading.TYPE.LINE_SCALE_PULSE_OUT
+    }); 
+
+    var sUrl = "https://xs01p2000138187trial.hanatrial.ondemand.com/fit-analysis/db-logic/registration_login.xsjs";
+    $.ajax({
+        type: "POST",
+        url: sUrl,
+        // cache: false,
+        data: JSON.stringify(oData),
+        // dataType: "json",
+        error: function (msg, textStatus) {
+            debugger;
+            alert("User Registration Failed");
+        },
+        success: function (data) {
+            debugger;
+            window.EasyLoading.hide();
+        }
+    }); // End of Ajax Call
+}
+
+
