f755480afd4682c0ea90da6f1acc24915d0d3ca9
arunkrishnamoorthy <arunmba.rec@gmail.com>
2018-03-03 17:13:17 +0530

    Jawbone Integration - Fitness App

--- js/Dashboard.js
+++ js/Dashboard.js
@@ -63,7 +63,46 @@ Dashboard.prototype.GetOToken = function(oAuth){
 
 Dashboard.prototype.updateSelectedDeviceData = function(){
     // Implement logic to get the OAuth Data for the selected device. 
+    var old_refresh_token = "-v54Cev6wyuM7BNV28fwN5bTmTMWoNQP8o28E63-8efdL2WxNgQvKekaCy5aBtavNNWfJhnfRQwlAN2iCODyqw";
+    var client_id = "MSsGZt9IUew";
+    var client_secret = "807ce7cf440e76f03477bc574c31363c93a9865e";
+    var grant_type="refresh_token";
+     
+    var callbackURL = window.location.origin + "/Callback.html";
+    var url = "https://jawbone.com/auth/oauth2/token?";
+    var path = "client_id=" + clientId + "&client_secret=" + + "&grant_type=" + grant_type + "&refresh_token=" + old_refresh_token; 
+    url = url + path;
 
-    var oAuth = "jF_LbmL3sgBFtTptYscH9BPpRbVH3Nos5gEKv9Z_POpNXBs12wVrpPVp7yiPhmgpW1OCWnRHe8gh7xZacFOon4H9bA56EPM0F3QduHr1dPyfYO5UpCruMKZqfMZ8c7MVTVmvNT1Jvh2mElEiCLaFPi4QM6t8bYfs9U0AdldD3GY5lYW7UzDvaCLrM2X9UO_PypyEIhDOPnXL4edZxb_S_lXRlI7-0LwK7pipyWc0OSMDgCYKc1ZBN6Q1S_sJ2fuDVTsORwQuXHMd9uFgtVYtNw";
-    this.GetOToken(oAuth);
+    $.ajax({
+        type: "GET",
+        url: url,
+        dataType: "json",
+        success: function(data,response){
+            debugger;
+            var access_token = data.access_token;
+            var refresh_token = data.refresh_token;
+            var token_type = data.token_type;
+            var url = "https://jawbone.com/nudge/api/v.1.1/users/@me/moves";
+            var auth_value = token_type + " " + access_token;
+            $.ajax({
+                type: "GET",
+                url: url,
+                dataType: "json",
+                beforeSend: function(xhr){
+                    xhr.setRequestHeader('Authorization', auth_value );
+                },
+                success: function(data,response){
+                    debugger;
+                    var items = data.data.items;
+                },
+                error: function(response){
+                    debugger;
+                }
+            });
+        },
+        error: function(response){
+                    debugger;
+        }
+    })
+
 }
