59e3d839892ac8c9d652464a8796c015b91a495b
arunkrishnamoorthy <arunmba.rec@gmail.com>
2018-03-03 08:15:03 +0530

    Jawbone Integration - Fitness App

---
+++ js/Dashboard.js
@@ -0,0 +1,21 @@
+/**
+ * Class Constructor
+ */
+function Dashboard(){
+    return this;
+}
+
+Dashboard.prototype.GetOAuth = function(url){
+    debugger;
+    window.EasyLoading.show({
+        type: EasyLoading.TYPE.LINE_SCALE_PULSE_OUT
+    }); 
+    window.open(url, "_blank", "location=yes");
+    window.EasyLoading.hide();
+}
+
+Dashboard.prototype.GetOToken = function(oAuth){
+    debugger;
+}   
+
+
