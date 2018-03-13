00c7a73b671315f9ea765bc30512be1a427bfc96
arunkrishnamoorthy <arunmba.rec@gmail.com>
2018-03-13 10:16:39 +0530

    Final Commit

--- js/Dashboard.js
+++ js/Dashboard.js
@@ -38,14 +38,14 @@ Dashboard.prototype.GetOToken = function (oAuth) {
               "SECRETKEY": client_secret,
               "REFRESHTOKEN": refresh_token
             };
-            // oData = JSON.stringify(oData);
+            oData = JSON.stringify(oData);
             $.ajax({
                type :"POST",
-               headers: { "Content-Type": "application/json" }, 
+              //  headers: { "Content-Type": "application/json" }, 
                url: sUrl,
                data: oData,
-               dataType: "json",
-               crossDomain: true,
+               dataType: "application/json",
+              //  crossDomain: true,
               //  contentType: "application/json",
                error: function (msg, textStatus) {
                 alert("Device Registered Succesfully");
